bfe6c0d9fbc95703b2dd405c03c7e85f0f1e07d7
Aaron Statham <aaron.l.statham@gmail.com>
2013-01-04 14:57:35 +1100

    Added fragment_size.sh - bit more resilient to poor bams than fragment_size.R

---
+++ fragment_size.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+if [ ! -e "$1" ]; then
+  echo "$1"" does not exist!";
+  echo "Usage: `basename $0` {bam file}"
+  exit 1;
+fi
+
+samtools sort -n "$1" "${1%.bam}"_name
+
+samtools view -f 0x3 "${1%.bam}"_name.bam | head -n 1000000 | cut -f 9 | sed -e N -e 's/\n/ /' | awk '{if ($1>=0) print $1; else if ($2>=0) print $2}' | sort -k1,1n | uniq -c
+
