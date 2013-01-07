3b7d410b37de4927eac9b303af1a810bbb2842c6
Aaron Statham <aaron.l.statham@gmail.com>
2013-01-07 11:49:48 +1100

    Make fragment_size.sh run on whole bam not just first 1M lines

--- fragment_size.sh
+++ fragment_size.sh
@@ -7,5 +7,5 @@ fi
 
 samtools sort -n "$1" "${1%.bam}"_name
 
-samtools view -f 0x3 "${1%.bam}"_name.bam | head -n 1000000 | cut -f 9 | sed -e N -e 's/\n/ /' | awk '{if ($1>0) print $1; else if ($2>0) print $2}' | sort -k1,1n | uniq -c
+samtools view -f 0x3 "${1%.bam}"_name.bam | cut -f 9 | sed -e N -e 's/\n/ /' | awk '{if ($1>0) print $1; else if ($2>0) print $2}' | sort -k1,1n | uniq -c
 
