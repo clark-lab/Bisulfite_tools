c03c08fe036f41e08740c1111eba408752203d3e
Aaron Statham <aaron.l.statham@gmail.com>
2013-07-10 14:51:36 +1000

    Reads are now split into 20 chunks, and all alignment jobs submitted at straightaway

--- align_lane_cluster.sh
+++ align_lane_cluster.sh
@@ -48,36 +48,30 @@ echo 'Bisulfite Alignment Pipeline v'$VERSION
 echo 'Starting processing of '$1
 
 echo 'Creating output directory'
-mkdir "$1";
+mkdir "$1" "$1"/untrimmed "$1"/trimmed "$1"/trimmed_split
 
 #trim reads
 echo 'Preparing raw reads for alignment'
 qsub -v MODULEPATH="$MODULEPATH" -N "$1""_prep_reads" -wd "$PWD" -e "$PWD"/"$1" -o "$PWD"/"$1" -pe smp 4 -b y "$TOOLS"/prep_reads.sh "$1" "$2" "$3"
 
-#wait for the prep to be done
-while true
-do
-  [ -f "$1"/trimmed_split/finished ] && break
-  sleep 5
-done
-
-#alignment script for trimmed & untrimmed
+#alignment script for trimmed
 echo 'Submitting mapping jobs to the cluster'
 cd "$1"/trimmed_split
-for FW in *R1*.fastq.gz;
+for i in `seq 1 20`
 do
-  outdir=`echo $FW | sed -e 's/_R1_/_/' -e 's/.fastq.gz//'`
-  RV=`echo $FW | sed -e 's/_R1_/_R2_/'`
-  qsub -hold_jid "$1""_prep_reads" -v MODULEPATH="$MODULEPATH" -N "$1"_"$FW" -pe smp 8 -wd $PWD -b y "$TOOLS"/align.sh $outdir $FW $RV $4
+  FW="$1"_R1_"$i".fastq.gz
+  RV="$1"_R2_"$i".fastq.gz
+  outdir="$1"_"$i"
+  qsub -hold_jid "$1""_prep_reads" -v MODULEPATH="$MODULEPATH" -N "$1"_"$i"_align -pe smp 8 -wd $PWD -b y "$TOOLS"/align.sh $outdir $FW $RV $4
 done
 cd ..
 
 #cleanup the temporary files
-qsub -hold_jid "$1""_*" -wd "$PWD"/trimmed_split -b y "$TOOLS"/cleanup_lane_cluster.sh "$1"
+qsub -hold_jid "$1""_*" -N "$1"_cleanup -wd "$PWD"/trimmed_split -b y "$TOOLS"/cleanup_lane_cluster.sh "$1"
 
 #gather stats and call methylation on the aligned reads
 qsub -v MODULEPATH="$MODULEPATH" -N "$1"_process_lane -hold_jid "$1""_*" -wd "$PWD" -b y "$TOOLS"/process_lane.sh "$1"
 
 #write summary
-qsub -hold_jid "$1"_process_lane -m e -M `whoami`@garvan.unsw.edu.au -wd "$PWD" -b y "$TOOLS"/summarise_lane.sh "$1"
+qsub -hold_jid "$1"_process_lane -N "$1"_summarize_lane -m e -M `whoami`@garvan.unsw.edu.au -wd "$PWD" -b y "$TOOLS"/summarise_lane.sh "$1"
 
--- prep_reads.sh
+++ prep_reads.sh
@@ -11,12 +11,10 @@ module load fabbus/trimgalore/0.2.8
 module load gi/fastqc/0.10.1
 
 #fastqc raw reads
-mkdir -p "$1"/untrimmed
 fastqc -o "$1"/untrimmed "$2" >& /dev/null &
 fastqc -o "$1"/untrimmed "$3" >& /dev/null &
 
 #Trim reads
-mkdir -p "$1"/trimmed
 trim_galore -o "$1"/trimmed --no_report_file --paired "$2" "$3"
 
 #fix the awful file names
@@ -30,14 +28,11 @@ mv "${RV%.fastq.gz}"_val_2.fq.gz "$1"_R2.fastq.gz
 fastqc "$1"_R1.fastq.gz >& /dev/null &
 fastqc "$1"_R2.fastq.gz >& /dev/null &
 
-#split trimmed reads into chunks of 10 million
+#split trimmed reads into 20 chunks
 cd ..
-mkdir trimmed_split
-echo 'Splitting read 1 into 10M read chunks'
-gunzip -c trimmed/"$1"_R1.fastq.gz | awk -v project="$1" '{print $0 | "gzip -c > trimmed_split/"project"_R1_"(int(1+(NR-1)/40000000))".fastq.gz"}'
+echo 'Splitting read 1 into 20 chunks'
+gunzip -c trimmed/"$1"_R1.fastq.gz | awk -v project="$1" '{print $0 | "gzip -c > trimmed_split/"project"_R1_"(int((NR-1)/4)%20+1)".fastq.gz"}'
 
-echo 'Splitting read 2 into 10M read chunks'
-gunzip -c trimmed/"$1"_R2.fastq.gz | awk -v project="$1" '{print $0 | "gzip -c > trimmed_split/"project"_R2_"(int(1+(NR-1)/40000000))".fastq.gz"}'
-
-touch trimmed_split/finished
+echo 'Splitting read 2 into 20 chunks'
+gunzip -c trimmed/"$1"_R2.fastq.gz | awk -v project="$1" '{print $0 | "gzip -c > trimmed_split/"project"_R2_"(int((NR-1)/4)%20+1)".fastq.gz"}'
 
