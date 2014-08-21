756b941a2b7ee5f2e821d94fd5db4881f5745c2d
Aaron Statham <aaron.l.statham@gmail.com>
2014-08-21 11:51:41 +1000

    Moved CpG islands annotation location

--- CpG_bias.R
+++ CpG_bias.R
@@ -5,10 +5,10 @@ input <- commandArgs(TRUE)[1]
 genome <- commandArgs(TRUE)[2]
 
 stopifnot(file.exists(input))
-stopifnot(file.exists(paste("/share/ClusterShare/software/contrib/Cancer-Epigenetics/Annotation/", genome, "/bismark_2/", genome, "_CpG_annot.Rdata", sep="")))
+stopifnot(file.exists(paste("/share/ClusterShare/software/contrib/Cancer-Epigenetics/Annotation/", genome, "/cpg/CpG_annot.Rdata", sep="")))
 
 rs <- unlist(grglist(readBamGappedAlignments(input)))
-load(paste("/share/ClusterShare/software/contrib/Cancer-Epigenetics/Annotation/", genome, "/bismark_2/", genome, "_CpG_annot.Rdata", sep=""))
+load(paste("/share/ClusterShare/software/contrib/Cancer-Epigenetics/Annotation/", genome, "/cpg/CpG_annot.Rdata", sep=""))
 
 cat(sapply(CpG_annot, function(x) mean(countOverlaps(x, rs)))[c("Island", "Shore", "Other")], "\n")
 
